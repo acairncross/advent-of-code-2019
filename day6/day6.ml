81d23882933d9e41c08e19fbd71d4cdcd64c5ed3
Aiken Cairncross <acairncross@gmail.com>
2019-12-07 23:12:13 +0000

    Day 6 (part 2)

--- day6/day6.ml
+++ day6/day6.ml
@@ -37,7 +37,33 @@ let rec count_orbits (Node(_, ts)) num_ancestors =
        ts
      + num_ancestors
 
+let rec find_some_in_list = function
+  | [] -> None
+  | None :: tl -> find_some_in_list tl
+  | Some(x) :: _ -> Some(x)
+
+let rec find_in_tree (Node(x, ts)) y =
+  if String.equal y x
+  then Some([])
+  else
+    let routes = List.map ~f:(fun t -> find_in_tree t y) ts in
+    Option.map ~f:(fun xs -> x :: xs) (find_some_in_list routes)
+
+let rec common_prefix xs ys =
+  match xs, ys with
+  | x :: xs, y :: ys -> if String.equal x y then x :: common_prefix xs ys else []
+  | _, _ -> []
+
 let _ =
   let orbit_tree = read_input |> fun child_map -> child_map_to_tree child_map "COM" in
   let num_orbits = count_orbits orbit_tree 0 in
-  Out_channel.printf "%d\n" num_orbits
+  Out_channel.printf "%d\n" num_orbits;
+  match
+    let open Option in
+    find_in_tree orbit_tree "YOU" >>= fun r1 ->
+    find_in_tree orbit_tree "SAN" >>= fun r2 ->
+    let prefix_length = List.length (common_prefix r1 r2) in
+    return ((List.length r1 - prefix_length) + (List.length r2 - prefix_length))
+  with
+  | None -> Out_channel.printf "Failed\n"
+  | Some(n) -> Out_channel.printf "%d\n" n
