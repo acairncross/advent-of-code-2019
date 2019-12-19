94b1c01dfd983a4cbd17a0eacf995d5f5e8589aa
Aiken Cairncross <acairncross@gmail.com>
2019-12-19 00:19:17 +0000

    Day 14 (part 1)

--- day14/day14.ml
+++ day14/day14.ml
@@ -3,16 +3,96 @@ open Stdio
 
 let div_ru x y = if Int.rem x y = 0 then x/y else x/y + 1
 
-(*
-let solve
-*)
+let reverse_dependencies reactions =
+  let add_mapping map key value =
+    Map.update map key ~f:(function
+        | None -> Set.singleton (module String) value
+        | Some(values) -> Set.add values value) in
+  let rec go reactions rev_deps =
+    match reactions with
+    | [] -> rev_deps
+    | (reactants, product) :: reactions ->
+       let rev_deps' =
+         List.fold_left
+           reactants
+           ~init:rev_deps
+           ~f:(fun map r -> add_mapping map r product) in
+       go reactions rev_deps'
+  in go reactions (Map.singleton (module String) "FUEL" (Set.empty (module String)))
+
+let find_undepended rev_deps_orig rev_deps =
+  let rec go = function
+    | [] -> None
+    | (product, dependants) :: tl ->
+       if Set.is_empty dependants
+       then Some(product, Map.find_exn rev_deps_orig product)
+       else go tl
+  in go (Map.to_alist rev_deps)
+
+(* Find how much of the reactant we need to make dependant_amount of the dependant *)
+let find_required_amount reactions_list reactant dependant dependant_amount =
+  let rec find_reaction = function
+    | [] -> failwith "couldn't find reaction"
+    | (_reactants, (_product_n, product_name)) as reaction :: tl ->
+       if String.equal product_name dependant
+       then reaction
+       else find_reaction tl in
+  let (reactants, (product_n, _)) = find_reaction reactions_list in
+  let rec find_reactant_n = function
+    | [] -> failwith "couldn't find reactant"
+    | (reactant_n, reactant_name) :: tl ->
+       if String.equal reactant_name reactant
+       then reactant_n
+       else find_reactant_n tl in
+  let reactant_n = find_reactant_n reactants in
+  (div_ru dependant_amount product_n) * reactant_n
+
+let rec solve reactions_list rev_deps_orig rev_deps required_amounts =
+  if Map.is_empty rev_deps
+  then Map.find_exn required_amounts "ORE"
+  else
+    match find_undepended rev_deps_orig rev_deps with
+    | None -> failwith "couldn't find undepended reactant"
+    | Some(reactant, dependants) ->
+       let rev_deps' =
+         Map.remove rev_deps reactant
+         |> Map.map ~f:(fun dependants -> Set.remove dependants reactant) in
+       let required_amount' =
+         if String.equal reactant "FUEL" then 1
+         else
+           Set.sum (module Int)
+             ~f:(fun dependant ->
+               find_required_amount
+                 reactions_list
+                 reactant
+                 dependant
+                 (Map.find_exn required_amounts dependant))
+             dependants
+         in
+       let required_amounts' =
+         Map.add_exn
+           required_amounts
+           ~key:reactant
+           ~data:required_amount' in
+       Out_channel.printf "required_amount': %d\n" required_amount';
+       solve reactions_list rev_deps_orig rev_deps' required_amounts'
 
 let () =
-  let reactions =
+  let reactions_list =
     let lexbuf = Lexing.from_channel stdin in
     Parser.reactions Lexer.read lexbuf in
+  let unquantified_reactions =
+    List.map
+      reactions_list
+      ~f:(fun (reactants, product) -> List.map reactants ~f:snd, snd product) in
+  let rev_deps = reverse_dependencies unquantified_reactions in
+  let solution = solve reactions_list rev_deps rev_deps (Map.empty (module String)) in
+  Out_channel.printf "Solution: %d\n" solution;
   (*
-  List.iter reactions ~f:(fun (inputs, (n, x)) ->
-                               List.iter inputs ~f:(fun (n, x) -> Out_channel.printf "%d %s\n" n x);
-                               Out_channel.printf "%d %s\n" n x)
+  Map.iteri
+    rev_deps
+    ~f:(fun ~key:key ~data:value ->
+      Out_channel.printf "%s: " key;
+      Set.iter value ~f:(fun v -> Out_channel.printf "%s " v);
+      Out_channel.printf "\n")
    *)
